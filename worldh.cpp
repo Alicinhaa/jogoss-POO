7c1d93cd8389406a570b92b291aa069b6bfe9766
alicinhaa <aliceoliveira.maso@gmail.com>
2016-06-08 21:06:54 -0300

    JOGOS DE POO
    ATIVIDADES

---
+++ worldh.cpp
@@ -0,0 +1,12 @@
+// abrindo biblioteca
+#include <iostream>
+using namespace std;
+
+//entando na funcao principal
+int main() {
+	// mandando imprimir na tela
+	cout << "H E L L O  W O R L D!" << endl;
+
+	//comando para finalizar o programa
+	return 0;
+}
